9bb2cb64d1c9dc8fd65dc805132d573165be696b
DJH <dan@halabe.com>
2017-05-03 07:42:14 -0700

    ready to apply editing

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,33 +3,26 @@
 require('normalize.css/normalize.css');
 require('styles/App.css');
 
-import Rational from './Rational';
-import Matrix from './Matrix';
-
 import React from 'react';
-import RationalComponent from './RationalComponent';
 import MatrixComponent from './MatrixComponent';
+
+import Matrix from './Matrix';
 
 class AppComponent extends React.Component {
     constructor() {
         super();
-        this.state = { rat: new Rational(3,4), mat: new Matrix(3,3), matR: new Matrix(3,3) };
-    }
-    onClick() {
-        let matR = this.state.matR.map((rat) => rat.add(new Rational(1,3)));
-        this.setState({ matR:matR });
+        let p = [ [0, 2, 8, -7], [2, -2, 4, 0], [-3, 4, -2, -5] ];
+        let mat = new Matrix(3,4,p);
+        let matR = mat.gaussJordan();
+        this.state = { mat: mat, matR: matR };
     }
     render() {
         return (
             <div>
-                <p>Rational test</p><br/><br/>
-                <RationalComponent value={ this.state.matR.at(0,0) }/>
-                <p>Matrix test</p>
+                <p>Here&#39;s a Matrix</p>
                 <MatrixComponent mat={ this.state.mat} />
-                <p>Update matrix test</p>
+                <p>Here it is with Gauss-Jordan applied:</p>
                 <MatrixComponent mat={ this.state.matR} />
-                <p>Test here</p>
-                <button onClick={this.onClick.bind(this)}>Test</button>
             </div>
         );
     }
